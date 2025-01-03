0e995df836f5802ee7ab106aa37722d75e389d90
choi-yeong <choi_yeong@naver.com>
2025-01-03 20:38:58 +0900

    input()과 자료형변환을 응용한 문제풀이까지 진행완료.

--- code/python/3.py
+++ code/python/3.py
@@ -1,3 +1,4 @@
+"""
 #입력함수 사용 테스트
 song = input("당신의 최애 노래는?")
 print(song)
@@ -12,7 +13,7 @@ print(a*2) #a를 2번 반복해버린다... (곱하기2를 하려는 의도였�
 # float("문자열") #문자열을 float 실수형으로 바꿔준다.
 print(float(a)*2) #float형이라서 소수점이 나오지만 int형으로 하면 소수점이 안나온다.
 print(int(a)*3)
-
+"""
 b=2 #정수형
 print(b*10)
 print(str(b)*10)
@@ -20,3 +21,14 @@ print(str(b)*10)
 # print(b + "입니다.") : int형 b와 str형 "입니다."는 서로 더하기(+)할수 없다.
 print(str(b)+"입니다.", " : str형 \"b\"와 str형 \"입니다.\"는 더하기로 서로 붙여줄수있다.") # str형 "b"와 str형 "입니다."는 더하기로 서로 붙여줄수있다.
 print(str(b)*10, " : str형 \"b\"와 int형 10을 곱해주면 str형 \"b\"를 10번 반복하겠다는 뜻이다.") #str형 "b"와 int형 10을 곱해주면 str형 "b"를 10번 반복하겠다는 뜻이다.
+
+# str() 활용 예시
+math_score=80
+eng_score=92
+total_score=math_score+eng_score
+avg_score=total_score/2
+
+print("합계 :",total_score) #콤마(,)의 경우 자료형이 다르더라도 값이 붙여써진다 대신 콤마(,)자리엔 띄어쓰기가 된다.
+print("평균 :", avg_score) 
+print("총점 : "+str(total_score)) #더하기(+)의 경우 자료형이 다르면 오류가 된다.
+print("평균 : "+str(avg_score))
---
+++ code/python/실습3.py
@@ -0,0 +1,10 @@
+#1번
+name = input("1.이름을 입력하세요.")
+age = input("1.나이를 입력하세요.")
+print(f"1.안녕하세요! {name}님 ({age})")
+
+#2번
+name2 = input("2.이름을 입력하세요.")
+born2 = input("2.태어난 년도를 입력하세요.")
+year2 = input("올해 년도를 입력하세요.")
+print(f"올해는{year2}년, {name2}님의 나이는 {int(year2)-int(born2)}세 입니다.")
